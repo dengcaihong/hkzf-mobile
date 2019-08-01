c2ae92b5609a2f9097d2e59854147e3696d3fbba
dengcaihong <dengcaihong77@163.com>
2019-08-01 22:17:40 +0800

    项目的环境搭建-路由-antd-sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import City from './pages/City'
+import Map from './pages/Map'
 
 class App extends React.Component {
   render() {
-    return <div className="app">app</div>
+    return (
+      <Router>
+        <div className="app">app</div>
+        <ul>
+          <li>
+            <Link to="/home">首页</Link>
+          </li>
+          <li>
+            <Link to="/city">城市选择</Link>
+          </li>
+          <li>
+            <Link to="/map">地图找房</Link>
+          </li>
+        </ul>
+
+        {/* 配置路由的规则 */}
+        <Route path="/home" component={Home}></Route>
+        <Route path="/city" component={City}></Route>
+        <Route path="/map" component={Map}></Route>
+      </Router>
+    )
   }
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd-mobile'
+
+class Home extends React.Component {
+  render() {
+    return <div>Home组件
+      <Button type="primary">这是一个按钮</Button>
+    </div>
+  }
+}
+
+export default Home
